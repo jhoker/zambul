6aec27bbb432e6b1dad037bd38ecaf0f086a3e57
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-17 20:58:29 +0700

    Update trialssws.sh

--- xray/trialssws.sh
+++ xray/trialssws.sh
@@ -1,6 +1,7 @@
 domain=$(cat /etc/xray/domain)
 tls="$(cat ~/log-install.txt | grep -w "Sodosok WS/GRPC" | cut -d: -f2|sed 's/ //g')"
 user=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
+cipher="aes-128-gcm"
 uuid=$(cat /proc/sys/kernel/random/uuid)
 masaaktif=1
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
@@ -19,23 +20,23 @@ clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\\E[0;41;36m        Sodosok WS/GRPC Account      \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Remarks 	  : ${user}"
-echo -e "Domain 	  : ${domain}"
-echo -e "Port TLS 	: ${tls}"
-echo -e "Port  GRPC : ${tls}"
-echo -e "Password 	: ${uuid}"
-echo -e "Cipers		  : aes-128-gcm"
-echo -e "Network	  : ws/grpc"
-echo -e "Path 		  : /ss-ws"
+echo -e "Remarks 	    : ${user}"
+echo -e "Domain 	    : ${domain}"
+echo -e "Port TLS 	  : ${tls}"
+echo -e "Port  GRPC   : ${tls}"
+echo -e "Password 	  : ${uuid}"
+echo -e "Cipers		    : aes-128-gcm"
+echo -e "Network	    : ws/grpc"
+echo -e "Path 		    : /ss-ws"
 echo -e "ServiceName 	: ss-grpc"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link Ws  	: ${shadowsockslink}"
+echo -e "Link Ws  	  : ${shadowsockslink}"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link Grpc 	: ${shadowsockslink1}"
+echo -e "Link Grpc 	  : ${shadowsockslink1}"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link JSON 	: http://${domain}/ss-$user.txt"
+echo -e "Link JSON  	: http://${domain}/ss-$user.txt"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Expired On : $exp"
+echo -e "Expired On   : $exp"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
