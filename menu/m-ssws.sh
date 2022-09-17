e52ca4f4ee4a37c9221e18e4d2072b83e21a8400
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-17 20:11:05 +0700

    Update m-ssws.sh

--- menu/m-ssws.sh
+++ menu/m-ssws.sh
@@ -25,8 +25,9 @@ echo -e "\E[0;100;33m      • SS WS/GRPC •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Create Account SS WS/GRPC "
-echo -e " [\e[36m•2\e[0m] Extending Account SS WS/GRPC Active Life "
-echo -e " [\e[36m•3\e[0m] Delete Account SS WS/GRPC "
+echo -e " [\e[36m•2\e[0m] Create Trial SS WS/GRPC "
+echo -e " [\e[36m•3\e[0m] Extending Account SS WS/GRPC Active "
+echo -e " [\e[36m•4\e[0m] Delete Account SS WS/GRPC "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e   ""
@@ -38,8 +39,9 @@ read -p " Select menu : " opt
 echo -e ""
 case $opt in
 1) clear ; add-ssws ;;
-2) clear ; renew-ssws ;;
-3) clear ; del-ssws ;;
+2) clear ; trialssws ;;
+3) clear ; renew-ssws ;;
+4) clear ; del-ssws ;;
 0) clear ; menu ;;
 x) exit ;;
 *) echo "salah tekan" ; sleep 1 ; m-ssws ;;
