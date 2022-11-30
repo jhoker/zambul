39429f5a757c6e3d7e5c8d1b473b4c611ee93af3
jhoker <jhokerhacker@gmail.com>
2022-12-01 00:57:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -95,21 +95,6 @@ echo -e "[ ${green}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
 apt install python -y >/dev/null 2>&1
 echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
-sleep 2
-echo -ne "[ ${green}INFO${NC} ] Check permission : "
-
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-green "Permission Accepted!"
-else
-red "Permission Denied!"
-rm setup.sh > /dev/null 2>&1
-sleep 10
-exit 0
-fi
 sleep 3
 
 mkdir -p /var/lib/SIJA >/dev/null 2>&1
