0344e29ef39615e70c572ad10f31b1c1caa42f72
jhoker <jhokerhacker@gmail.com>
2022-12-05 04:23:43 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -51,7 +51,7 @@ touch /var/log/xray/error2.log
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 # Installation Xray Core
 # $latest_version
-xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v1.5.9/xray-linux-64.zip"
+xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v1.6.5/Xray-linux-64.zip"
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version
 
 
